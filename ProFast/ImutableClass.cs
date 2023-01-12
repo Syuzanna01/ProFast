8bc7d798fb34367c43ed865e0c3e8788481440bb
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2023-01-12 22:30:28 +0400

    Add project files.

---
+++ ProFast/ImutableClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProFast
+{
+    internal class ImutableClass
+    {
+        public readonly string name ;
+
+        public int Age { get; }
+        public ImutableClass(string name,int age)
+        {
+            this.name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ ProFast/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ProFast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 688;
+            double num2 = num1;
+            byte num3 = (byte)num1;
+            
+            if (int.TryParse(Console.ReadLine(),out int b))
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine("it is not a number!!");
+            }
+
+            ImutableClass imutable = new ImutableClass("Syuzi",1948);
+        }
+    }
+}
