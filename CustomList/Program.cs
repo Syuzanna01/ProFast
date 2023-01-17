10924fbfabf2aadf0d0dade2d68ebb7cf60eebdd
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2023-01-17 23:59:36 +0400

    .

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -1,70 +1,50 @@
-﻿//namespace CustomList
-//{
-//    class MyList<T> : List<T>
-//    {
+﻿using System.Collections;
+using System.Threading.Channels;
 
-//    }
+namespace CustomList
+{
+    class MyList<T> : IEnumerable
+    {
+        private List<T> items = new List<T>();
+        private int count = 0;
 
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            MyList<int> myList = new MyList<int>();
-//            myList.Add(1);
-//            myList.Add(2);
-//            myList.Add(3);
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
 
-//            foreach (int item in myList)
-//            {
-//                Console.WriteLine(item);
-//            }
+        public T Get(int index)
+        {
+            return items[index];
+        }
 
-//            Console.WriteLine("Hello, World!");
-//        }
-//    }
-//}
-//using System.Collections;
-//using System.Threading.Channels;
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>() { 1, 8, 6, 11 };
+            myList.Add(1);
+            myList.Add(9);
+            myList.Add('A');
 
-//namespace CustomList
-//{
-//    class MyList<T> : IEnumerable<T>
-//    {
-//        private T[] items = new T[10];
-//        private int count = 0;
-
-//        public void Add(T item)
-//        {
-//            items[count++] = item;
-//        }
-
-//        public IEnumerator<T> GetEnumerator()
-//        {
-//            for (int i = 0; i < count; i++)
-//            {
-//                yield return items[i];
-//            }
-//        }
-
-//        IEnumerator IEnumerable.GetEnumerator()
-//        {
-//            return GetEnumerator();
-//        }
-//    }
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            MyList<int> myList = new MyList<int>();
-//            myList.Add(1);
-//            myList.Add(9);
-//            myList.Add('A');
-
-//            foreach (int item in myList)
-//            {
-//                Console.WriteLine(item);
-//            }
-//        }
-//    }
-//}
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
 
